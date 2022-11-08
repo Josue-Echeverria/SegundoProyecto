90b930223d392e554d2bfb61938e677912eec339
deylan sandoval <deylan6@gmail.com>
2022-11-08 11:22:36 -0600

    fdf

--- src/main/java/com/mycompany/segundoproyecto/configuracion/VentanaJuego.java
+++ src/main/java/com/mycompany/segundoproyecto/configuracion/VentanaJuego.java
@@ -230,11 +230,11 @@ public class VentanaJuego extends javax.swing.JFrame {
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
 
                 .addComponent(lbPerdiste, javax.swing.GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE))
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDisponiblesLayout.createSequentialGroup()
+                //.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDisponiblesLayout.createSequentialGroup()
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btJugar)
-                .addContainerGap())
+                //.addContainerGap())
 
         );
         pnlDisponiblesLayout.setVerticalGroup(
